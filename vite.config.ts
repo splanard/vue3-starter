114231bed3730091e738b6616eddb2f6852d9c76
splanard <splanard@softwaymedical.fr>
2023-01-11 23:22:37 +0100

    🔧 Properly configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    cache: {
+      dir: fileURLToPath(new URL('./node_modules/.vitest', import.meta.url)),
+    },
+  },
+});
