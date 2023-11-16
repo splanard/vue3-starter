e98dfe760a95ef2f2c5470e3bc2ef499ef24d21b
Sébastien PLANARD <splanard@softwaymedical.fr>
2023-11-16 11:44:22 +0100

    ⬆️ Upgrade npm dependencies

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,6 +1,6 @@
+import HelloWorld from '@/components/HelloWorld.vue';
 import { shallowMount, VueWrapper } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
-import HelloWorld from '../HelloWorld.vue';
 
 let wrapper: VueWrapper;
 function mountComponent(name: string): void {
